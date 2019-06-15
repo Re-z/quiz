d05efd61b6c702843b30f147784232f225b4ecbc
re-z <re-z@ukr.net>
2019-06-15 18:05:31 +0300

    finished

--- js/script.js
+++ js/script.js
@@ -4,8 +4,7 @@ document.addEventListener('DOMContentLoaded', () => {
     totalIncorrectAnswers = getItemFromLS('totalIncorrectAnswers'),
     totalCorrectBlock = document.querySelector('.total__correct'),
     totalIncorrectBlock = document.querySelector('.total__incorrect'),
-    tptalQuestionsAmmount = 10;
-
+    totalQuestionsAmmount = 10;
 
     totalCorrectBlock.innerHTML = totalCorrectAnswers;
     totalIncorrectBlock.innerHTML = totalIncorrectAnswers;
@@ -13,61 +12,49 @@ document.addEventListener('DOMContentLoaded', () => {
 function getItemFromLS(name) {
     let data = localStorage.getItem(name);
     if (data) return data;
-    return 0
+    return 0;
 
 }
-
 
 // alert(totalCorrectBlock)
 async function getQuestion() {
     let query = await fetch('https://opentdb.com/api.php?amount=1&difficulty=easy'),
         answer = await query.json();
-        // console.log(queryJson)
         return {
             answer
         }
-
-
 }
-// getQuestion();
-
 
 function printQuestion() {
     let quest = getQuestion();
     quest.then(data => {
         return data.answer.results;
     }).then(dataNext => {
-        // console.log(dataNext)
-        result.innerHTML = ''; //clear previous result 
+        result.innerHTML = ''; //clear previous result
             let htmlTemplate = `
-                <h2>${dataNext[0].question}</h2>
-                <p>${dataNext[0].category}</p>
+                <div style="width: 100%">
+                    <h2>${dataNext[0].question}</h2>
+                    <p>${dataNext[0].category}</p>
+                </div>
             `
             result.innerHTML += htmlTemplate;
             
             let answers = dataNext[0].incorrect_answers,
                 correctAnswer = dataNext[0].correct_answer;
-            answers.push(correctAnswer); ;// create all questions array
+            answers.push(correctAnswer); // create all questions array
             answers.sort(() => Math.random() - 0.5);
-            // console.log(answers)
             answers.forEach(el => {
                
-                let htmlTemplate = `<button class="btn btn-info col-5 col-md-5 mt-1">${el}</button>`;
+                let htmlTemplate = `<button class="btn btn-info col-sm-12 col-md-5 mt-1">${el}</button>`;
                 result.innerHTML += htmlTemplate;
-            })
-            
-            
+            });
             markAnswer();
             checkAnswer(correctAnswer);
-
-
     })
 }
-if(questionCounter >= tptalQuestionsAmmount) {
-    showGameOver();
-} else {
-    printQuestion();
-}
+if(questionCounter >= totalQuestionsAmmount)
+{ showGameOver(); }
+else { printQuestion(); }
 
 function markAnswer() {
     let answerBtn = document.querySelectorAll('.btn-info');
@@ -78,44 +65,37 @@ function markAnswer() {
                 if(el.classList.contains('btn-warning')) {
                     el.classList.remove('btn-warning')
                 }
-            })
+            });
             ev.target.classList.add('btn-warning');
         })
     })
 }
 
-function checkAnswer(correctAnswer, funcRemoveEventListener) {
-    // result.removeEventListener('click', funcRemoveEventListener);
+function checkAnswer(correctAnswer) {
 
     check.addEventListener('click', () =>  {
         let chosenAnswerBtn = document.querySelector('.btn-warning');
         if(chosenAnswerBtn) {
             let chosenAnswer = chosenAnswerBtn.textContent;
             if(chosenAnswer === correctAnswer) {
-                console.log('Correct');
                 totalCorrectAnswers++;
                 totalCorrectBlock.innerHTML = totalCorrectAnswers;
     
             } else {
-                console.log('Bad answer');
                 totalIncorrectAnswers++;
                 totalIncorrectBlock.innerHTML = totalIncorrectAnswers;
-    
             }
             questionCounter++;
             localStorage.setItem('totalCorrectAnswers', totalCorrectAnswers);
             localStorage.setItem('totalIncorrectAnswers', totalIncorrectAnswers);
             localStorage.setItem('questionCounter', questionCounter);
     
-            if(questionCounter >= tptalQuestionsAmmount) {
-                showGameOver()
-            } else {
-                printQuestion();
-            }
+            if(questionCounter >= totalQuestionsAmmount) { showGameOver() }
+            else { printQuestion(); }
         } else {
-            alert('1111')
+            alert('1111');
+            checkAnswer(correctAnswer)
         }
-        
 
     },{once: true}) // detach event from being repeated 
 }
@@ -129,12 +109,11 @@ function resetStats() {
 resetBtn.addEventListener('click', resetStats);
 
 function showGameOver() {
-    let htmlTemplate = `<div class="bg-info col-md-12">
-        <h2>The game is end</h2>
-        <p>Total correct answers - ${totalCorrectAnswers}</p>
-        <p>Total incorrect answers -  ${totalIncorrectAnswers}</p>
-
-    
+    let htmlTemplate = `
+    <div class="bg-secondary rounded col-md-12">
+        <h2>Game over</h2>
+        <p>Total correct answers - <span class="text-success font-weight-bold">${totalCorrectAnswers}</span></p>
+        <p>Total incorrect answers -  <span class="text-danger font-weight-bold">${totalIncorrectAnswers}</span></p>
     </div>`
     check.classList.add('disabled')
     result.innerHTML = htmlTemplate
